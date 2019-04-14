9d8d93207e6af551d3a7ef46f8b385ce913f38eb
Rushit Doshi <doshi.rs@gmail.com>
2019-04-14 11:41:08 -0700

    Added chalk, debug and morgan dependencies

--- library/app.js
+++ library/app.js
@@ -1,11 +1,16 @@
 var express = require('express');
+var chalk = require('chalk');
+var debug = require('debug')('app');
+var morgan = require('morgan');
 
 var app = express();
 
+app.use(morgan('tiny'));
+ 
 app.get('/', function(req, res){
     res.send('Hello from my library app');
-});
+}); 
 
 app.listen(3000, function(){
-    console.log('listening on port 3000');
+    debug(`listening on port ${chalk.green('3000')}`);
 });
