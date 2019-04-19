cac9eaa6b86cade0d4a24628cd17bf8cbf8e7849
Rushit Doshi <doshi.rs@gmail.com>
2019-04-18 19:24:23 -0700

    Added nodemon and related config

--- library/app.js
+++ library/app.js
@@ -6,6 +6,7 @@ const morgan = require('morgan');
 const path = require('path');
 
 const app = express();
+const port = process.env.PORT || 3000;
 
 app.use(morgan('tiny'));
 app.use(express.static(path.join(__dirname, 'public')));
@@ -18,6 +19,6 @@ app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'views/index.html'));
 });
 
-app.listen(3000, () => {
-  debug(`listening on port ${chalk.green('3000')}`);
+app.listen(port, () => {
+  debug(`listening at port ${chalk.green(port)}`);
 });
