9cc2868fe7e92a7430bb0d76dfd9f2867a012b11
Rushit Doshi <doshi.rs@gmail.com>
2019-04-19 15:47:49 -0700

    Added Pug and EJS templates

--- library/app.js
+++ library/app.js
@@ -1,4 +1,3 @@
-/* eslint-disable linebreak-style */
 const express = require('express');
 const chalk = require('chalk');
 const debug = require('debug')('app');
@@ -13,10 +12,14 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/css', express.static(path.join(__dirname, '/node_modules/bootstrap/dist/css')));
 app.use('/js', express.static(path.join(__dirname, '/node_modules/bootstrap/dist/js')));
 app.use('/js', express.static(path.join(__dirname, '/node_modules/jquery/dist')));
+app.set('views', './src/views');
+// app.set('view engine', 'pug');
+app.set('view engine', 'ejs');
 
 app.get('/', (req, res) => {
   // res.send('Hello from my library app');
-  res.sendFile(path.join(__dirname, 'views/index.html'));
+  // res.sendFile(path.join(__dirname, 'views/index.html'));
+  res.render('index', { list: ['a', 'b'], title: 'Library' });
 });
 
 app.listen(port, () => {
